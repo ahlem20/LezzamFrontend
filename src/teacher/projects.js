8302634ffb117138474baad1e9758cfd0eaeea6e
ahlem20 <107472826+ahlem20@users.noreply.github.com>
2024-09-09 11:11:34 +0100

    Update projects.js

--- src/teacher/projects.js
+++ src/teacher/projects.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import { API_URL } from '../config/constants';
+
 const NonActiveProjectsTable = () => {
     const user = JSON.parse(localStorage.getItem('lezzam')); // Get user from localStorage
     const [projects, setProjects] = useState([]);
